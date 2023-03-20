40af8a09a133c08124c3020812b234a5c672a04f
lucab <lucab@DESKTOP-BGFMTHJ.station>
2023-03-20 15:36:07 +0100

    Merge branch 'master' of https://github.com/LucaNocella1993/oracle-certified-associate-java-se-8-rogrammer-1Z0-808.git

--- src/oca/exercises/TestClass.java
+++ src/oca/exercises/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 		return null;}
 	
     public static void main(String[] args) {
-        List al = new ArrayList(); //1
+        List<Integer> al = new ArrayList<>(); //1
         al.add(111); //2
         System.out.println(al.get(al.size()));  //3
      }
